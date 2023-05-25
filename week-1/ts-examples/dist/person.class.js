efd28eccebd91433555f54c74d448c018ed59742
Ngi Bujri <nbujri7@gmail.com>
2023-05-24 20:40:24 -0400

    complete assignment 1.4

---
+++ week-1/ts-examples/dist/person.class.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Person {
+    constructor(firstName, lastName) {
+        (this.firstName = firstName), (this.lastName = lastName);
+    }
+}
+let myName = new Person("Ngi", "Bujri");
+console.log(`My name is ${myName.firstName} ${myName.lastName}`);
