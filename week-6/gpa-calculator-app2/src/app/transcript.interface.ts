15b38893a31fa60e5636ca828ab630e7e4eb50d5
Ngi Bujri <nbujri7@gmail.com>
2023-07-07 14:26:37 -0400

    complete assignment 6.4

---
+++ week-6/gpa-calculator-app2/src/app/transcript.interface.ts
@@ -0,0 +1,11 @@
+/**
+ * title: transcript.interface.ts
+ * author: ngi bujri
+ * date: july 6 2023
+ * description: transcript interface
+ */
+
+export interface ITranscript {
+  course: string;
+  grade: string;
+}
